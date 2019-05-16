2747fe32e55164923f040be7b3c2f8cb208fa99e
Kanzari <kanzari.amin@gmail.com>
2019-05-16 14:06:04 +0100

    Updates

---
+++ precache-manifest.91cd6e14cbbd04f3e05d40c6b3d8dff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a9b6dfe2c0ed41fbe05",
+    "url": "/React-Calculator/static/js/runtime~main.4a9b6dfe.js"
+  },
+  {
+    "revision": "0efe0309975ae0262989",
+    "url": "/React-Calculator/static/js/main.0efe0309.chunk.js"
+  },
+  {
+    "revision": "b672d7ae8d243b5b90bd",
+    "url": "/React-Calculator/static/js/1.b672d7ae.chunk.js"
+  },
+  {
+    "revision": "0efe0309975ae0262989",
+    "url": "/React-Calculator/static/css/main.dbd79d00.chunk.css"
+  },
+  {
+    "revision": "1962d7ad1889f18a1a2c46d46fc239eb",
+    "url": "/React-Calculator/index.html"
+  }
+];
